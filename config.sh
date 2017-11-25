da7df42fbc9023fb163860992fa091cd0b53adc9
TheDevFreak <24739570+TheDevFreak@users.noreply.github.com>
2017-11-25 08:30:38 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -42,7 +42,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "   Virtual Home Button GWatch  "
   ui_print "*******************************"
 }
 
